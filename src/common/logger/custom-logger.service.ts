ace44c7158d7b2df769a1b5869f60253098b8a1b
romina-rodriguez <rominarodriguez@ug.uchile.cl>
2023-02-10 03:22:31 -0300

    Add tests for AppController and AppService

--- src/common/logger/custom-logger.service.ts
+++ src/common/logger/custom-logger.service.ts
@@ -1,4 +1,4 @@
 import { Injectable, Scope, ConsoleLogger } from '@nestjs/common';
 
 @Injectable({ scope: Scope.TRANSIENT })
-export class CustomLogger extends ConsoleLogger {}
+export class CustomLogger extends ConsoleLogger {} //TODO: Add custom config
