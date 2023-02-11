7ae5e16c23cd20dc54578689afbadd3bc3f8336f
romina-rodriguez <rominarodriguez@ug.uchile.cl>
2023-02-11 02:03:14 -0300

    Add ApiOperation and ApiResponse decorators to AppController

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 import { AppService } from './app.service';
 
@@ -9,6 +9,14 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Returns a welcome message',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Shows user a welcome message',
+    type: Object,
+  })
   getData() {
     return this.appService.getData();
   }
