585a37f085d32cca2c191ecdab8539c69032fc37
romina-rodriguez <rominarodriguez@ug.uchile.cl>
2023-02-13 13:39:41 -0300

    Add examples to the ResponseTaskDto and improve attributes class validations

--- src/resources/todo/dto/response-task.dto.ts
+++ src/resources/todo/dto/response-task.dto.ts
@@ -4,22 +4,25 @@ import {
   IsString,
   IsBoolean,
   IsNumber,
-  IsDate,
+  IsMongoId,
+  IsDateString,
 } from 'class-validator';
 import mongoose from 'mongoose';
 
 export class ResponseTaskDto {
   @ApiProperty({
     description: 'Unique mongo id identifier of the new task',
+    example: '63e3b3d44ed62e2f3c747952',
     type: String,
     required: true,
   })
   @IsNotEmpty()
-  @IsString()
+  @IsMongoId()
   _id: mongoose.Types.ObjectId;
 
   @ApiProperty({
     description: 'New task added to the checklist',
+    example: 'Sleep',
     type: String,
     required: true,
   })
@@ -29,6 +32,7 @@ export class ResponseTaskDto {
 
   @ApiProperty({
     description: 'Boolean that indicates wether the task is done or not',
+    example: false,
     type: Boolean,
     required: true,
   })
@@ -38,33 +42,37 @@ export class ResponseTaskDto {
 
   @ApiProperty({
     description: 'Timestamp that indicates when the task was created',
-    type: Date,
+    example: '2023-02-08T14:38:12.679Z',
+    type: String,
     required: true,
   })
   @IsNotEmpty()
-  @IsDate()
-  createdAt: Date;
+  @IsDateString()
+  createdAt: string;
 
   @ApiProperty({
     description: 'Timestamp that indicates when the task was updated',
-    type: Date,
+    example: '2023-02-10T12:40:29.831Z',
+    type: String,
     required: true,
   })
   @IsNotEmpty()
-  @IsDate()
-  updatedAt: Date;
+  @IsDateString()
+  updatedAt: string;
 
   @ApiProperty({
     description: 'Timestamp that indicates when the task was soft deleted',
-    type: Date,
+    example: '2023-02-10T12:40:29.831Z',
+    type: String,
     required: true,
   })
   @IsNotEmpty()
-  @IsDate()
-  isDeleted: Date;
+  @IsDateString()
+  isDeleted: string;
 
   @ApiProperty({
     description: 'Number that indicates the version key of the task object',
+    example: 0,
     type: Number,
     required: true,
   })
